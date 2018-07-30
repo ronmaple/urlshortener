59a5ad02657d6b89a9afde076e39af0aff0e5c36
Glitch (scarce-prawn) <none>
2018-07-30 04:09:01 +0000

    📷😹 Checkpoint ./server.js:1216088/5381

--- server.js
+++ server.js
@@ -27,52 +27,56 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get("/api/hello", (req, res) => res.json({ greeting: 'hello API' }) );
 
 app.post("/api/shorturl/new", (req, res) => {
-    let url = req.body.url;
-    console.log('url', url);
-    
-    dns.lookup(url, (err, address, family) => {
-      console.log('dns.lookup. address:', address, 'url', url);
-        if (address !== undefined) {
-          console.log('address !== undefined');
-            UrlEntry
-                .find({ originalUrl: url})
-                .exec( (err, data) => {
-                    
-                    if (err) console.log(err);
-              
-                    if (data.length == 0) {
-                        console.log('data not in db', data);
+  let postURL = req.body.url;
+  console.log('posturl', postURL);
+  
+  let indexHTTPS = postURL.indexOf('https://' || 'http://') == undefined ? 0 : postURL.indexOf('https://' || 'http://');
+  let url = postURL.splice(0, postURL);
+  
+  
+  dns.lookup(url, (err, address, family) => {
+    console.log('dns.lookup. address:', address, 'url', url);
+      if (address !== undefined) {
+        console.log('address !== undefined');
+          UrlEntry
+              .find({ originalUrl: url})
+              .exec( (err, data) => {
 
-                        UrlEntry.find({}, (err, data) => {
-                            if (err) console.log(err);
+                  if (err) console.log(err);
 
-                            let entry = new UrlEntry({
-                                id: data.length,
-                                originalUrl: url
-                            });
+                  if (data.length == 0) {
+                      console.log('data not in db', data);
 
-                            entry.save();
-                            res.json({
-                                original_url: entry.originalUrl,
-                                count: entry.id
-                            })
-                        })
-                    } else {
-                      console.log('data in db', data);
-                      UrlEntry
-                        .find({ originalUrl: url})
-                        .exec((err, data) => {
+                      UrlEntry.find({}, (err, data) => {
                           if (err) console.log(err);
-                        
+
+                          let entry = new UrlEntry({
+                              id: data.length,
+                              originalUrl: url
+                          });
+
+                          entry.save();
                           res.json({
-                            original_url: data[0].originalUrl,
-                            count: data[0].id
+                              original_url: entry.originalUrl,
+                              count: entry.id
                           })
+                      })
+                  } else {
+                    console.log('data in db', data);
+                    UrlEntry
+                      .find({ originalUrl: url})
+                      .exec((err, data) => {
+                        if (err) console.log(err);
+
+                        res.json({
+                          original_url: data[0].originalUrl,
+                          count: data[0].id
                         })
-                    }
-                })
-        }
-    });
+                      })
+                  }
+              })
+      }
+  });
 })
 
 const port = process.env.PORT || 3000;
